cbbb04aaa39d67673b9475937e9199ccbff48626
fronox <serjzv@gmail.com>
2019-03-31 14:04:00 +0300

    Search is added

--- VisualCardsMVC/Controllers/CardController.cs
+++ VisualCardsMVC/Controllers/CardController.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -11,16 +12,23 @@ namespace VisualCardsMVC.Controllers
         static HttpClient _client = new HttpClient();
         private static string _path = "http://localhost:8000/api/cards";
 
-        public async Task<IActionResult> ViewCard(int cardId)
-        {
-            var card = await HttpGetCard(cardId);
-            return View(card);
-        }
-
         public async Task<IActionResult> EditCard(int cardId)
         {
             var card = await HttpGetCard(cardId);
             return View(card);
+        }
+        
+        public IActionResult AddCard(int columnId)
+        {
+            var card = new CardModel(0, "", "", columnId);
+            return View(card);
+        }
+        
+        public async Task<IActionResult> UpdateCard(int cardId, string title, string description, int columnId)
+        {
+            var card = new CardModel(cardId, title, description, columnId);
+            await _client.PutAsJsonAsync($"{_path}/{cardId}", card);
+            return RedirectToAction("Columns", "Column");
         }
         
         public async Task<CardModel> HttpGetCard(int id)
@@ -34,42 +42,29 @@ namespace VisualCardsMVC.Controllers
             return card;
         }
 
-        public async Task<CardModel> HttpPostCard(CardModel cardModel)
-        {
-            var response = await _client.PostAsJsonAsync($"{_path}", cardModel);
-            CardModel newCard = null;
-            if (response.IsSuccessStatusCode)
-            {
-                newCard = await response.Content.ReadAsAsync<CardModel>();
-            }
-
-            return newCard;
-        }
-
-        public async Task<IActionResult> DeleteCard(int cardId)
+        public async Task<IActionResult> HttpDeleteCard(int cardId)
         {
             await _client.DeleteAsync($"{_path}/{cardId}");
             return RedirectToAction("Columns", "Column");
         }
-
-        public async Task<IActionResult> UpdateCard(int cardId, string title, string description, int columnId)
-        {
-            var card = new CardModel(cardId, title, description, columnId);
-            await _client.PutAsJsonAsync($"{_path}/{cardId}", card);
-            return RedirectToAction("Columns", "Column");
-        }
-
-        public IActionResult AddCard(int columnId)
-        {
-            var card = new CardModel(0, "", "", columnId);
-            return View(card);
-        }
         
-        public async Task<IActionResult> SaveCard(string title, string description, int columnId)
+        public async Task<IActionResult> HttpSaveCard(string title, string description, int columnId)
         {
             var card = new CardModel(0, title, description, columnId);
             await _client.PostAsJsonAsync($"{_path}", card);
             return RedirectToAction("Columns", "Column");
+        }
+        
+        public async Task<IActionResult> SearchCardByTitle(string searchTitle)
+        {
+            var cardList = new List<CardModel>();
+            var resp = await _client.GetAsync($"{_path}/search?title={searchTitle}");
+            if (resp.IsSuccessStatusCode)
+            {
+                cardList = await resp.Content.ReadAsAsync<List<CardModel>>();
+            }
+
+            return View(cardList);
         }
     }
 }
